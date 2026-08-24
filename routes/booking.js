750a2be764e0c6d6a2c38f0abfce551f79952d85
Anamika Pandey <pandeyanamika841@gmail.com>
2026-08-24 17:11:58 +0530

    fix: standardize isLoggedIn export naming

--- routes/booking.js
+++ routes/booking.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true }); // mergeParams to get :id from listing route
 const wrapAsync = require("../utils/wrapAsync");
-const { isLoggedIn } = require("../middleware");
+const { isloggedIn } = require("../middleware");
 const bookingController = require("../controller/booking");
 
-router.post("/", isLoggedIn, wrapAsync(bookingController.createBooking));
-router.get("/my-bookings", isLoggedIn, wrapAsync(bookingController.myBookings));
-router.delete("/:bookingId", isLoggedIn, wrapAsync(bookingController.cancelBooking));
+router.post("/", isloggedIn, wrapAsync(bookingController.createBooking));
+router.get("/my-bookings", isloggedIn, wrapAsync(bookingController.myBookings));
+router.delete("/:bookingId", isloggedIn, wrapAsync(bookingController.cancelBooking));
 
 module.exports = router;
